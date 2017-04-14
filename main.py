db8e3ca77629ba77bcb349f3e274345a65dc3c97
Jaurron Crawford <jaurroncrawford@yahoo.com>
2017-04-13 20:05:06 -0500

    worked on templates and main page structure.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
-class Art(db.Model):
+class Post(db.Model):
 	title = db.StringProperty(required = True)
-	art = db.TextProperty(required = True)
+	post = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
 
 class Handler(webapp2.RequestHandler):
@@ -25,29 +25,65 @@ class Handler(webapp2.RequestHandler):
 		self.write(self.render_str(template, **kw))
 		
 		
-class MainPage(Handler):
-	def render_front(self, title="", art="", error=""):
-		arts = db.GqlQuery("SELECT * FROM Art "
+class Index(Handler):
+
+	blog_posts = db.GqlQuery("SELECT * FROM Movie WHERE ")
+	
+	def get(self):
+		t = jinja_env.get_template("front.html")
+        content = t.render()
+        self.response.write(content)
+
+			
+class Blog(Handler):
+	def render_blog(self, title="", post="", error=""):
+		posts = db.GqlQuery("SELECT * FROM Post "
 							"ORDER BY created DESC ")
 	
-		self.render("front.html", title=title, art=art, error=error, arts=arts)
+		self.render("front.html", title=title, post=post, error=error, posts=posts)
 
 	def get(self):
 		self.render_front()
 		
 	def post(self):
 		title = self.request.get("title")
-		art = self.request.get("art")
+		post = self.request.get("post")
 		
-		if title and art:
-			a = Art(title = title, art = art)
+		if title and post:
+			a = Post(title = title, post = post)
 			a.put()
 			
 			self.redirect("/")
 		else:
-			error = "we need both a title and some artowrk!"
-			self.render_front(title, art, error = error)
+			error = "we need both a title and a blog post!"
+			self.render_front(title, post, error = error)
+
+class NewPosts(Handler): 
+	def render_newposts(self, title="", post="", error=""):
+		posts = db.GqlQuery("SELECT * FROM Post "
+							"ORDER BY created DESC ")
+							
+		self.render("newposts.html", title=title, post=post, error=error, posts=posts)
+	
+	def get(self):
+		self.render_newposts()
+		
+	def post(self):
+		title = self.request.get("title")
+		post = self.request.get("post")
+		
+		if title and post:
+			a = Post(title = title, post = post)
+			a.put()
+			
+			self.redirect("/blog")
+		else:
+			error = "You need to put in both a title and some content"
+			self.render_front(title, post, error = error)
+
  
 app = webapp2.WSGIApplication([
-	('/', MainPage)
+	('/', Index),
+	('/blog', Blog),
+	('/newpost', NewPosts)
 ], debug=True)
